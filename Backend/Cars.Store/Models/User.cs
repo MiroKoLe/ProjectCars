b26e8ccb5dcc55d3e222a50715925fe183d34ab8
MiroKoLe <miroslavkolev@gmail.com>
2021-11-16 21:46:21 +0100

    Bunch of weird code.

--- Backend/Cars.Store/Models/User.cs
+++ Backend/Cars.Store/Models/User.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Cars.Store.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public string FirstName{ get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
