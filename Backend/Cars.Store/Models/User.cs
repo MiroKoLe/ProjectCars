99149cd646e36d1127cf08d765064c1947fad850
MiroKoLe <miroslavkolev@gmail.com>
2021-11-16 21:05:20 +0100

    Refactoring User model. now inheriting from IdentityUser.

--- Backend/Cars.Store/Models/User.cs
+++ Backend/Cars.Store/Models/User.cs
@@ -6,10 +6,7 @@ namespace Cars.Store.Models
 {
     public class User
     {
-       public int Id { get; set; }
-       public string Email { get; set; }
-       public string Password { get; set; }
-       public string UserName { get; set; }
-       public  string DateOfRegistration { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
     }
 }
